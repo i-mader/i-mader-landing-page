7a113f22b6fe021c68aaf850f13ec77eb5795901
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2024-06-07 00:20:49 +0700

    fix icon, image, skills, components

--- src/app/ui/swiper-basic-2.tsx
+++ src/app/ui/swiper-basic-2.tsx
@@ -14,57 +14,75 @@ import Image from "next/image";
 const dataSkills = [
   {
     name: "Javascript",
+    img: "/images/skills/js.svg",
   },
   {
     name: "Typescript",
+    img: "/images/skills/icon-typescript.svg",
   },
   {
     name: "React",
+    img: "/images/skills/icon-react.svg",
   },
   {
     name: "Next.js",
+    img: "/images/skills/icon-nextjs.svg",
   },
   {
     name: "Node.js",
+    img: "/images/skills/icon-nodejs.svg",
   },
   {
     name: "Express.js",
+    img: "/images/skills/icon-express.svg",
   },
   {
     name: "Nest.js",
+    img: "/images/skills/icon-nest.svg",
   },
   {
     name: "Socket.io",
+    img: "/images/skills/icon-socket.svg",
   },
   {
     name: "Golang",
+    img: "/images/skills/Go-Logo_Blue 1.svg",
   },
   {
     name: "PostgreSQL",
+    img: "/images/skills/icon-postgresql.svg",
   },
   {
     name: "MongoDB",
+    img: "/images/skills/icon-mongodb.svg",
   },
   {
     name: "Sass/Scss",
+    img: "/images/skills/icon-sass.svg",
   },
   {
     name: "Tailwindcss",
+    img: "/images/skills/icon-tailwindcss.svg",
   },
   {
     name: "Figma",
+    img: "/images/skills/icon-figma.svg",
   },
   {
     name: "Cypress",
+    img: "/images/skills/icon-cypress.svg",
   },
   {
     name: "Storybook",
+    img: "/images/skills/icon-storybook.svg",
   },
   {
     name: "Git",
+    img: "/images/skills/icon-git.svg",
   },
   {
     name: "MySQL",
+    img: "/images/skills/mysql 1.svg",
   },
 ];
 
@@ -98,12 +116,7 @@ const SwiperBasic2 = () => {
             <SwiperSlide key={item.name}>
               <div className="flex flex-col items-center justify-center">
                 <div className="flex items-center justify-center w-16 h-16">
-                  <Image
-                    src="/images/skills/js.svg"
-                    alt="img-1"
-                    width={50}
-                    height={50}
-                  />
+                  <Image src={item.img} alt="img-1" width={35} height={35} />
                 </div>
                 <p className="mt-2">{item.name}</p>
               </div>
